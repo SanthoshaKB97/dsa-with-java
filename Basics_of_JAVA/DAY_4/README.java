7012cb13b76e61d533c2c96567fe2b5d37dc4de4
Santhosha K B <santhuvirat03@gmail.com>
2026-06-12 19:01:42 +0530

    Update README.java

--- Basics_of_JAVA/DAY_4/README.java
+++ Basics_of_JAVA/DAY_4/README.java
@@ -1,1 +1,15 @@
 
+byte: 100
+short: 30000
+int: 100000
+long: 10000000000
+float: 3.14
+double: 3.14159265359
+char: A
+boolean: true
+String: Hello, Java
+Array: 1 2 3 4 5 
+Wrapper Integer: 50
+StringBuilder: Java
+
+=== Code Execution Successful ===
