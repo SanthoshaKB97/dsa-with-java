29ea83c5f7a52b850ead7b105b6b7e2e01adf8a5
Santhosha K B <santhuvirat03@gmail.com>
2026-06-14 18:11:19 +0530

    Update Hello_world.java

--- Basics_of_JAVA/Day_1/Hello_world.java
+++ Basics_of_JAVA/Day_1/Hello_world.java
@@ -1,12 +1,12 @@
 
 public class HelloWorld {
     
-    // Your program begins with a call to main()
+    // Your program begins with a call to main() // its name; //
     
     public static void main(String[] args)
     {
-        // Prints "Hello, World" to the terminal window.
+        // Prints "Hello, World" to the terminal window.// main is the program method..//
         
-        System.out.println("Hello, World");
+        System.out.println("Hello APPA AMMA");   // screen output(code output.)//
     }
 }
