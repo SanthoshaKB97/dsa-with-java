61bdc7fbb1495cf2bb7887620465b9e0d36edc57
Santhosha K B <santhuvirat03@gmail.com>
2026-06-15 16:55:37 +0530

    Update Conditional Statements.java

--- Basics_of_JAVA/DAY_6/Conditional Statements.java
+++ Basics_of_JAVA/DAY_6/Conditional Statements.java
@@ -1,1 +1,20 @@
 
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name= sc.nextLine();
+        
+        System.out.print("Enter your age: ");
+        int age= sc.nextInt();
+        
+        System.out.println("MIR_" +name);
+        if (age >=18) {
+            System.out.println("Eligible to vote");
+        }else {
+            System.out.println("Not eligible");
+        }
+        sc.close();
+    }
+}
