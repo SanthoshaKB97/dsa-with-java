0b96e7c7545727f92b63e749a0ad9150488036d2
Santhosha K B <santhuvirat03@gmail.com>
2026-06-15 17:37:47 +0530

    Update switch.java

--- Basics_of_JAVA/DAY_6/switch.java
+++ Basics_of_JAVA/DAY_6/switch.java
@@ -1,1 +1,16 @@
 
+public class Main{
+    public static void main(String[]args){
+        String Gread ="B+";
+        switch(Gread){
+        case "A+":
+        System.out.println("Exelent");
+        break;
+        case "B+":
+        System.out.println("very good");
+        break;
+        default:
+        System.out.println("Invalid Gread");
+        }
+    }
+}
