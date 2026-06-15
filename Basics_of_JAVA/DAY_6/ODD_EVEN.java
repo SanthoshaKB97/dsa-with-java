9b6d292dec904029c95eba8ce46451a91c3377e2
Santhosha K B <santhuvirat03@gmail.com>
2026-06-15 18:02:58 +0530

    Update ODD_EVEN.java

--- Basics_of_JAVA/DAY_6/ODD_EVEN.java
+++ Basics_of_JAVA/DAY_6/ODD_EVEN.java
@@ -1,1 +1,16 @@
 
+import java.util.Scanner;
+public class Main{
+    public static void main(String [] args){
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter youre number:   ");
+        int num =sc.nextInt();
+        
+        if(num % 2==0){
+        System.out.println("EVEN");
+        } else{
+            System.out.println("ODD");
+        }
+    }
+}
