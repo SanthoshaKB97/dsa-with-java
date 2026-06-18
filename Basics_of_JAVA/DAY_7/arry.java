1e4fb5bba14942216504c4e6e2549f6d2b1ee60e
Santhosha K B <santhuvirat03@gmail.com>
2026-06-18 11:17:46 +0530

    Update arry.java

--- Basics_of_JAVA/DAY_7/arry.java
+++ Basics_of_JAVA/DAY_7/arry.java
@@ -1,1 +1,10 @@
 
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+ public class Main {
+    public static void main(String[] args) {
+    int[] numbers = {10, 20, 30, 40, 50};
+    System.out.println(numbers[3]);
+    }
+}
